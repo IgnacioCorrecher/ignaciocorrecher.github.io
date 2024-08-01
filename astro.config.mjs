e95ef3b88823887240cb6d589f108989843ca2cf
IgnacioCorrecher <ignaciocorrecher@hotmail.com>
2024-08-01 10:57:17 +0200

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://ignaciocorrecher.github.io',
-    base: '/ignaciocorrecher.github.io/',
+    base: '/',
 });
